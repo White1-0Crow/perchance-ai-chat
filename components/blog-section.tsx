0567314cc4284c0ffedfe280ee791935c13d9561
mofang2018 <35266909+mofang2018@users.noreply.github.com>
2024-11-24 15:43:46 +0800

    Update blog-section.tsx

--- components/blog-section.tsx
+++ components/blog-section.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link"
-import { Card, CardContent, CardFooter, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { motion } from 'framer-motion'
 
